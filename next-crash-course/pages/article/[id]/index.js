0905192238392c242967bdfeee212c4b0725730a
jrii1512 <jriimala@gmail.com>
2022-06-08 16:32:56 +0300

    Next crash course

---
+++ next-crash-course/pages/article/[id]/index.js
@@ -0,0 +1,53 @@
+
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+
+const article = ({article}) => {
+  //const router = useRouter()
+  //const {id} = router.query
+  console.log("props.article:", article)
+  return (
+      <div>
+          <h1>{article.title}</h1>
+          <p>{article.body}</p>
+          <br/>
+          <Link href='/'>Go Back</Link>
+      </div>
+    )
+}
+
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  )
+  const article = await res.json()
+
+  return{
+    props: {
+    article,
+    },
+  }
+}
+
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`
+  )
+
+  const articles = await res.json()
+  console.log("ARTICLES:", articles)
+  const ids = articles.map(article => article.id)
+  const paths = ids.map(id => ({params: {id: id.toString()}}))
+
+  return {
+    paths,
+    fallback : false
+  }
+
+
+
+
+}
+
+export default article
